a6ad83bef282029b5a43ba8c0a8dbdc7c98969ee
Christopher Kim <christopher.kim@originate.com>
2021-04-04 11:47:36 -0700

    Integrate bastion module updates

--- basic/terraform/environment/bastion.tf
+++ basic/terraform/environment/bastion.tf
@@ -1,11 +1,24 @@
+resource "aws_ecr_repository" "bastion" {
+  name = "${var.stack}-${terraform.workspace}-bastion"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Terraform   = "true"
+    Stack       = var.stack
+    Environment = terraform.workspace
+  }
+}
+
 module "bastion" {
   source = "github.com/originate/terraform-modules/custom/kubernetes_bastion"
 
-  profile = var.profile
-  stack   = var.stack
-  env     = terraform.workspace
+  ssh_port             = var.bastion_ssh_port
+  repo_url             = aws_ecr_repository.bastion.repository_url
+  docker_login_command = "eval '$(aws ecr get-login --no-include-email --profile ${var.profile})'"
 
-  ssh_port = var.bastion_ssh_port
 
   # Ensures all bastion host Kubernetes resources get deleted before removing
   # the cluster during a destroy operation
